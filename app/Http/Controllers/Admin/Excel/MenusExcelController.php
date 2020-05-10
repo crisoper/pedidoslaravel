cc661acca6f5d41757ebba7908418b41d8175800
crisoper <crisoper@gmail.com>
2020-05-09 20:52:51 -0500

    first commit

---
+++ app/Http/Controllers/Admin/Excel/MenusExcelController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Excel;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MenusExcelController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Admin/Pdf/MenusPdfController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Pdf;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MenusPdfController extends Controller
+{
+    //
+}
