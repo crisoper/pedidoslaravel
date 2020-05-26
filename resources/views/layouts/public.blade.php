f907fcfa5fa0c0a15f058a0e908b2a4195479480
GFLORES23 <54048385+GFLORES23@users.noreply.github.com>
2020-05-25 19:55:15 -0500

    actualizacion public

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -115,6 +115,12 @@
                                     <a class="nav-link" href="{{ route('register') }}"> Afilia a tu restaurante</a>
                                     
                                 </div>
+
+                        {{-- AFILIAR RESTAURANTE --}}
+                        <div class="col-6 col-sm-6 col-md-3 col-lg-5">
+                            <div class="header__top__titulo text-center">
+                                <div>
+                                    <a class="text-primary" href="#">Afilia a tu restaurante</a>
                                 </div>
                             </div>
                         </div>
