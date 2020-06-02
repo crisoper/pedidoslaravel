e3b3823be0c49c65f6b25cff6e592848b0b543b3
crisoper <crisoper@gmail.com>
2020-06-01 22:13:25 -0500

    Jobs

---
+++ app/Jobs/ProcesssendmailJob.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\User as UserModel;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Mail\Publico\ActivarcuentaempresaMail;
+
+class ProcesssendmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct( UserModel $user )
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        
+        try {
+            Mail::to( $this->user->email )
+            ->cc("crisoper@gmail.com")
+            ->send( new  ActivarcuentaempresaMail( $this->user ) );
+        } catch (\Exception $e) {
+            return null;
+        }
+        
+    }
+}
